08cb6d2569898ed8491f7796c26d49fb7edbeef2
Airtix <159016374+Airtix@users.noreply.github.com>
2024-03-25 12:25:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# suikajzz
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Airtix/suikajzz)
---
+++ fruits.js
@@ -0,0 +1,57 @@
+export const FRUITS = [
+  {
+    label: "golf",
+    radius: 40 / 2,
+    color: "#F20306",
+  },
+  {
+    label: "volant",
+    radius: 50 / 2,
+    color: "#FF624C",
+  },
+  {
+    label: "hand",
+    radius: 72 / 2,
+    color: "#A969FF",
+  },
+  {
+    label: "basket",
+    radius: 85 / 2,
+    color: "#FFAF02",
+  },
+  {
+    label: "foot",
+    radius: 106 / 2,
+    color: "#FC8611",
+  },
+  {
+    label: "rugby",
+    radius: 137 / 2,
+    color: "#F41615",
+  },
+  {
+    label: "gant",
+    radius: 155 / 2,
+    color: "#FDF176",
+  },
+  {
+    label: "casque",
+    radius: 170 / 2,
+    color: "#FEB6AC",
+  },
+  {
+    label: "baseball",
+    radius: 155 / 2,
+    color: "#F7E608",
+  },
+  {
+    label: "bowling",
+    radius: 250 / 2,
+    color: "#89CE13",
+  },
+  {
+    label: "medaille",
+    radius: 260 / 2,
+    color: "#26AA1E",
+  },
+];
